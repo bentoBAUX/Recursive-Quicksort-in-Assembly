4620bac88eec9b93d270fdd5e9ce79b706598866
bentoBAUX <bennettpoh@gmail.com>
2024-11-07 22:36:21 +0100

    First working commit
    Have page faults to debug

---
+++ quicksort.c
@@ -0,0 +1,69 @@
+#include <stdio.h>
+
+extern void quicksort_asm(int *array, size_t low, size_t high);
+void quicksort_c(int *array, size_t low, size_t high);
+
+int main()
+{
+    int arrayC[] = {53, 87, 22, 45, 9, 78, 32, 14, 68, 23};
+    int arrayASM[] = {53, 87, 22, 45, 9, 78, 32, 14, 68, 23};
+    int lengthC = sizeof(arrayC) / sizeof(arrayC[0]);
+    int lengthASM = sizeof(arrayASM) / sizeof(arrayASM[0]);
+
+    printf("C: ");
+
+    quicksort_c(arrayC, 0, lengthC - 1);
+
+    for (int i = 0; i < lengthC; i++)
+    {
+        printf("%d ", arrayC[i]);
+    }
+
+    printf("\n");
+
+    printf("ASM: ");
+    quicksort_asm(arrayASM, 0, lengthASM - 1);
+    for (int i = 0; i < lengthASM; i++)
+    {
+        printf("%d ", arrayASM[i]);
+    }
+
+    printf("\n");
+
+
+    return 0;
+}
+
+void quicksort_c(int *array, size_t low, size_t high)
+{
+    if (low >= high)
+    {
+        return;
+    }
+    int pivot = array[low];
+    int i = low - 1;
+    int j = high + 1;
+    while (1)
+    {
+        do
+        {
+            i++;
+        } while (array[i] < pivot);
+
+        do
+        {
+            j--;
+        } while (array[j] > pivot);
+
+        if (i >= j)
+        {
+            break;
+        }
+        int temp = (int)array[i];
+        array[i] = (int)array[j];
+        array[j] = temp;
+    }
+
+    quicksort_c(array, low, j);
+    quicksort_c(array, j + 1, high);
+}
